2c5c44ad0acac4e0b8b36142c835f6c0fc9d3ace
Joe Gomez <jcggomez@live.com>
2020-05-12 18:08:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,10 @@ def introduction name
 
 introduction ("Joe")
 
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 
-
+introduction_with_language
 
 
 
